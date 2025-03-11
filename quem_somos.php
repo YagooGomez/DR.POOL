9c9a68d63e8794b13c8f1753311e3a3cebe0c788
tomasfonseca <tomas.fonseca2005@gmail.com>
2025-03-11 14:24:11 +0000

    dfghj

--- quem_somos.php
+++ quem_somos.php
@@ -19,9 +19,14 @@
 
   <!-- Vendor CSS Files -->
   <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
-  
-  <!-- Template Main CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
 
+  <!-- Template Main CSS Files -->
+  <link href="assets/css/variables.css" rel="stylesheet">
   <link href="assets/css/main.css" rel="stylesheet">
 
 
